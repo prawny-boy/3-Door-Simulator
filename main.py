4354ba56107859edf49339c7ee79aceaabdb42c8
Sean Chan <sean.chan3@student.education.wa.edu.au>
2024-09-12 15:52:49 +0800

    Made a function that chooses which doors to reveal so that i can use it later added a round number printing with colour Added prefix option to the limited input function

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import random
 import sys
+sys.dont_write_bytecode = True
 import PrintFunctions
+from termcolor import cprint, colored
 
 # Variables
 numberOfGoodDoors = 1
+unknownDoorAmount = 2
 
 # Functions
 def GenerateRoom(doorNum:int, donkeyCount:int):
@@ -16,13 +19,29 @@ def GenerateRoom(doorNum:int, donkeyCount:int):
     print(doorDict)
     return doorDict
 
-def RevealDoor(doorRevealCount, chosenDoor):
-    pass
+def RevealDoor(doorRevealCount:int, chosenDoor:int, roomDict:dict):
+    revealableDoors = list(roomDict.keys())
+    revealableDoors.remove(chosenDoor)
+    doorsRevealed = []
+    for i in range(doorRevealCount):
+        while True:
+            revealed = random.choice(list(revealableDoors))
+            if roomDict[revealed] == 0:
+                break
+            else:
+                list(revealableDoors).remove(revealed)
+                continue
+        list(revealableDoors).remove(revealed)
+        doorsRevealed.append(revealed)
+    return doorsRevealed
 
 def UserSimulation(doorNum:int, simulation_times:int):
     for i in range(simulation_times):
+        cprint("#Round"+str(i), "green")
         roomDict = GenerateRoom(doorNum, doorNum-numberOfGoodDoors)
         chosenDoor = int(PrintFunctions.LimitedInput(list(roomDict.keys()), "Pick a door"))
+        doorsToBeRevealed = RevealDoor(doorNum-unknownDoorAmount, chosenDoor, roomDict)
+        print(doorsToBeRevealed)
 
 # Menu
 
